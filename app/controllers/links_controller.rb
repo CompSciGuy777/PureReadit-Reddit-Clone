f72f8b897e4c5454d336da9d32349457f7a1c910
whitelion777 <compsciguy777@gmail.com>
2015-11-29 02:52:08 +0000

    Finished Stage 1 Development

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -64,13 +64,13 @@ class LinksController < ApplicationController
 
   def upvote
     @link = Link.find(params[:id])
-    @link.upvote_by_current_user
+    @link.upvote_by current_user
     redirect_to :back
   end
   
   def downvote
      @link = Link.find(params[:id])
-    @link.downvote_by_current_user
+    @link.downvote_by current_user
     redirect_to :back
   end
 
