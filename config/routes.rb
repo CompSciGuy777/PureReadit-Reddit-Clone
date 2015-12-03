ee2ad9c4a7d47976bfa274bb4fc85656a5dc67b0
whitelion777 <compsciguy777@gmail.com>
2015-12-03 10:35:43 +0000

    Added Logo, Page Controller, and More Content

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,15 @@ Rails.application.routes.draw do
     end
     resources :comments
   end
+  
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'links#index'
+   root 'pages#homepage'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
